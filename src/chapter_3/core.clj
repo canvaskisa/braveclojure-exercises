6b9a5723cb5143550b92cab4bbe5f717a92ee464
Aleksandr Yakunichev <canvaskisa@gmail.com>
2017-01-15 23:52:48 +0300

    Initial commit

---
+++ src/chapter_3/core.clj
@@ -0,0 +1,6 @@
+(ns chapter-3.core)
+
+(defn foo
+  "I don't do a whole lot."
+  [x]
+  (println x "I am chapter 3!"))
---
+++ src/chapter_4/core.clj
@@ -0,0 +1,6 @@
+(ns chapter-4.core)
+
+(defn foo
+  "I don't do a whole lot."
+  [x]
+  (println x "I am chapter 4!"))
