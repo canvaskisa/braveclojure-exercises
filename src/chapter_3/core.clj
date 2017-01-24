e71569167d80b137b52cd91c42525b783805a199
Aleksandr Yakunichev <canvaskisa@gmail.com>
2017-01-24 22:23:09 +0300

    Add TODO for Exercise 3.6

--- src/chapter_3/core.clj
+++ src/chapter_3/core.clj
@@ -164,3 +164,11 @@
                                          (matching-weird-part part (str (* 72 5)))])))
           []
           asym-body-parts))
+
+;; ----------
+;; @TODO Exercise 6.
+;; Create a function that generalizes symmetrize-body-parts and the function you created in Exercise 5.
+;; The new function should take a collection of body parts and the number of matching body parts to add.
+;; If you’re completely new to Lisp languages and functional programming, it probably won’t be obvious how to do this.
+;; If you get stuck, just move on to the next chapter and revisit the problem later.
+;; ----------
