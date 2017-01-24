ef13b1c9b5ade562704fefe22c4c05968ad7a554
Aleksandr Yakunichev <canvaskisa@gmail.com>
2017-01-24 22:23:47 +0300

    Add Chapter 4 summary exercise

--- src/chapter_4/core.clj
+++ src/chapter_4/core.clj
@@ -1,6 +1,42 @@
 (ns chapter-4.core)
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "I am chapter 4!"))
+;; --------------------------------------------------------------
+;; Chapter 4 http://www.braveclojure.com/core-functions-in-depth/
+;;
+;; A Vampire Data Analysis Program for the FWPD
+
+(def filename "src/chapter_4/suspects.csv")
+
+(def vamp-keys [:name :glitter-index])
+
+(defn str->int
+  "Converts string to Integer"
+  [str]
+  (Integer. str))
+
+(def conversions {:name identity
+                  :glitter-index str->int})
+
+(defn convert
+  [vamp-key value]
+  ((get conversions vamp-key) value))
+
+(defn parse
+  "Convert a CSV into rows of columns"
+  [string]
+  (map #(clojure.string/split % #",")
+       (clojure.string/split string #"\n")))
+
+(defn mapify
+  "Return a seq of maps like {:name \"Edward Cullen\" :glitter-index 10}"
+  [rows]
+  (map (fn [unmapped-row]
+         (reduce (fn [row-map [vamp-key value]]
+                   (assoc row-map vamp-key (convert vamp-key value)))
+                 {}
+                 (map vector vamp-keys unmapped-row)))
+       rows))
+
+(defn glitter-filter
+  [minimum-glitter records]
+  (filter #(>= (:glitter-index %) minimum-glitter) records))
